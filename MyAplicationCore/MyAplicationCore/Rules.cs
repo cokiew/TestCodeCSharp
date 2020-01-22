56c14767f8a2b86525f0751d2e1629bd22c3a6c9
Jorge Uziel Hernández Luis <uziel_h_l@hotmail.com>
2020-01-22 00:47:31 -0600

    Nueva clase agregada

---
+++ MyAplicationCore/MyAplicationCore/Rules.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAplicationCore
+{
+    class Rules
+    {
+        public int AlertColor { get; set; }
+        public List<int> PointColorsArray { get; set; }
+    }
+}
