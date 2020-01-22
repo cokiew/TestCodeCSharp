0f48820e6b6135b9a11907c878182b06d4db6cea
Jorge Uziel Hernández Luis <uziel_h_l@hotmail.com>
2020-01-22 00:25:25 -0600

    Project in C# loaded

---
+++ MyAplicationCore/MyAplicationCore/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyAplicationCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RuleResponse> rules = new List<RuleResponse>();
+            List<RuleResult> ruleResults = new List<RuleResult>();
+            PointColors[] po = new PointColors[5];
+
+            List<double> deviationPoints = new List<double> { 1.73, 1.056, 1.257, 1.498, 1.003 };
+            List<double> deviationValues = new List<double> { 0.397, 1.73, 1.056, 1.257, 1.498,
+                                                              1.003, 1.003, 0.604, 0.512, 0.516,
+                                                              0.525, 0.66 };
+            int i =0;
+            foreach (double d in deviationPoints)
+            {
+                
+                PointColors points = new PointColors()
+                {
+                    PointColor = 2,
+                    PointValue = d
+                };
+                po[i] = points;
+                i++;
+            }
+
+            int count = deviationValues.Count();
+
+            RuleResult rule = new RuleResult()
+            {
+                AlertColor = 2,
+                PointColorsArray = po
+            };
+
+            ruleResults.Add(rule);
+
+            double[] piecesToPaint = new double[count];
+
+            foreach (RuleResult rul in ruleResults) {
+
+                List<PointColors> pointsColor = (from v in rul.PointColorsArray
+                                                 select v).ToList();
+
+
+                foreach (PointColors pnclr in pointsColor)
+                {
+                    int index = deviationValues.IndexOf(pnclr.PointValue);
+                    if (index != -1)
+                    {
+                        piecesToPaint[index] = pnclr.PointColor;
+                    }
+                }
+
+                RuleResponse finalResponse = new RuleResponse()
+                {
+                    AlertColor = rul.AlertColor,
+                    PointColorsPaint = piecesToPaint
+                };
+                rules.Add(finalResponse);
+            } //End foreach Rules
+        }
+    }
+}
---
+++ MyAplicationCore/MyAplicationCore/RuleResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAplicationCore
+{
+    public class RuleResult
+    {
+        public int AlertColor { get; set; }
+        public PointColors[] PointColorsArray { get; set; } = new PointColors[0];
+    }
+
+
+    public class PointColors
+    {
+        public int PointColor { get; set; }
+        public double PointValue { get; set; }
+    }
+
+    public class RuleResponse
+    {
+        public int AlertColor { get; set; }
+        public double[] PointColorsPaint { get; set; } = new double[0];
+    }
+}
